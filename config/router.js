d1f1d0b6ff79a4ef98f5e732259cdbf2d2daa246
Otto Sipe <ottosipe@gmail.com>
2012-11-01 23:47:32 -0400

    added logs file, removed junk

--- config/router.js
+++
@@ -1,3 +0,0 @@
-const CONTROLLER_PATH = __dirname + "/../app/controllers";
-
-module.exports = function Router(Server) {   }
--- config/views.js
+++
@@ -1,7 +0,0 @@
-const VIEW_PATH = __dirname + "/../app/views";
-
-module.exports = function(Server) {
-  Server.set("view engine", "jade");							// Use Jade by default
-  Server.set("views", VIEW_PATH);									// Set the base views path
-  Server.set("view options", { layout: false });	// Set the view options
-}
