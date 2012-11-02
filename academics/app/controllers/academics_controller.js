043ead10809d045bde8ca9baeca477eb9910e1d2
Otto Sipe <ottosipe@gmail.com>
2012-11-01 23:41:45 -0400

    added routes and controller functions

--- academics/app/controllers/academics_controller.js
+++ academics/app/controllers/academics_controller.js
@@ -3,10 +3,33 @@ var API = module.exports = exports;
 
 API.terms = function terms(req, res) {
 	res.send("Terms");
+	
 }
 
 API.schools = function schools(req, res) {
 	res.send("Schools");
 }
 
+API.departments = function departments(req, res) {
+	res.send("Departments");
+}
 
+API.courses = function courses(req, res) {
+	res.send("Courses");
+}
+
+API.sections = function sections(req, res) {
+	res.send("Sections");
+}
+
+API.times = function times(req, res) {
+	res.send("Times");
+}
+
+API.instructors = function instructors(req, res) {
+	res.send("Instructors");
+}
+
+API.descriptions = function descriptions(req, res) {
+	res.send("Descriptions");
+}
--- academics/config/router.js
+++ academics/config/router.js
@@ -5,5 +5,12 @@ var Academics = require(CONTROLLER_PATH + "/academics_controller");
 module.exports = function Router(Server) {   
 	
 	Server.get("/terms", Academics.terms);
+	Server.get("/:term_id/schools", Academics.schools);
+	Server.get("/:term_id/:school_id/departments", Academics.departments);
+	Server.get("/:term_id/:dept_id/courses", Academics.courses);
+	Server.get("/:term_id/:dept_id/:course_id/sections", Academics.sections);
+	Server.get("/:term_id/:dept_id/:course_id/:section_id/times", Academics.times);
+	Server.get("/:term_id/:dept_id/:course_id/:section_id/instructors", Academics.instructors);
+	Server.get("/:term_id/:dept_id/:course_id/descriptions", Academics.descriptions);
 
 }
